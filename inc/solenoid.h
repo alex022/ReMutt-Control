8482d2223a1dd9ec18b8d4c9d15ba285d28dd1ef
ericbru <eric_brunnett@celab3.ece.ucsb.edu>
2014-05-13 11:03:05 -0700

    solenoid valve functions

---
+++ inc/solenoid.h
@@ -0,0 +1,20 @@
+/*
+ * solenoid.h
+ *
+ *  Created on: May 13, 2014
+ *      Author: eric_brunnett
+ */
+
+#ifndef SOLENOID_H_
+#define SOLENOID_H_
+
+#include "gpio.h"
+
+#define SOLENOID_PORT	PORT4
+#define SOLENOID_PIN	19
+
+void initSolenoid();
+void solenoidOpen();
+void solenoidClose();
+
+#endif /* SOLENOID_H_ */
---
+++ src/solenoid.c
@@ -0,0 +1,26 @@
+/*
+ * solenoid.c
+ *
+ *  Created on: May 13, 2014
+ *      Author: eric_brunnett
+ */
+
+
+#include "solenoid.h";
+
+
+void initSolenoid()
+{
+	GPIOSetDir(SOLENOID_PORT,SOLENOID_PIN, GPIO_OUTPUT);
+	GPIOSetDir(SOLENOID_PORT,SOLENOID_PIN, 1);
+}
+
+void solenoidOpen()
+{
+	GPIOSetDir(SOLENOID_PORT,SOLENOID_PIN, 0);
+}
+
+void solenoidClose()
+{
+	GPIOSetDir(SOLENOID_PORT,SOLENOID_PIN, 1);
+}
