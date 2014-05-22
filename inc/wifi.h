7988a9dc96c5bb5e6da9b50c81886a60e52c2f6c
unknown <stevenguan@celab3.ece.ucsb.edu>
2014-05-22 10:51:00 -0700

    updated wifi.h forgotten with last commit

--- inc/wifi.h
+++ inc/wifi.h
@@ -10,12 +10,34 @@
 
 #include "uart.h"
 
-/* Signal Macros */
+/* WiFi Macros */
 #define AUTO_CONNECT 1
 #define CMD_MODE 2
 #define MAX_SIZE 50
+#define WIFI_BUFF_SIZE 10
+
+/* State Variable, buffer iterator */
+int STATE, iterator;
+
+/* Flag to check for a new connection */
+int check_flag;
+int message;
+
+/* Buffer to hold individual messages */
+char buffer[WIFI_BUFF_SIZE];
+
+/* Enumeration for state */
+enum{
+	IDLE, CONNECTED
+};
+
+/* Enumeration for message */
+enum{
+	START, OPEN, CLOSED
+};
 
 /* Function prototypes */
 void initWiFi(int setting);
+void getMessage(char string[]);
 
 #endif /* WIFI_H_ */
